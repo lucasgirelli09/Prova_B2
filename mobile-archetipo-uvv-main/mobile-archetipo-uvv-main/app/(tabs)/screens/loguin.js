b1208ec25818896dea734b36005ae0e6146d7e41
LUCAS GIRELLI BEZERRA <127257764+lucasgirelli09@users.noreply.github.com>
2024-11-18 10:35:13 -0300

    Add files via upload

---
+++ mobile-archetipo-uvv-main/mobile-archetipo-uvv-main/app/(tabs)/screens/loguin.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text } from 'react-native';
+import supabase from './supabaseClient';
+
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = async () => {
+    const { user, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      navigation.navigate('Home');  // Navegar para a tela inicial
+    }
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+      {error && <Text>{error}</Text>}
+    </View>
+  );
+};
+
+export default LoginScreen;
